17c049a603c7bb2a62d4084f2de8f1f73084f4b0
Clip&GPT事務局 <clipngpt@gmail.com>
2025-02-15 11:52:51 +0900

    backup

---
+++ _config/_task_ai/_sounds/_etc/2/0919_05_echo第3水曜掃除.bat
@@ -0,0 +1,13 @@
+@echo off
+set yyyy=%date:~0,4%
+set mm=%date:~5,2%
+set dd=%date:~8,2%
+set time2=%time: =0%
+set hh=%time2:~0,2%
+set mn=%time2:~3,2%
+set dt=%yyyy%%mm%%dd%%hh%%mn%
+
+echo ja,google,‘æ‚RT‚Ì…—j“ú‚ÍA•P˜H‰c‹ÆŠ‚ÌƒNƒŠ[ƒ“ƒAƒbƒvŽÀŽ{“ú‚Å‚·B > "c:\RiKi_assistant\temp\s6_5tts_txt\%dt%_1_sjis.txt"
+echo ja,google,‘å“c‚³‚ñA¼–{‚³‚ñAŽO“ç‚³‚ñA‚PŠK‚ð’†S‚ÉA´‘|‚æ‚ë‚µ‚­‚¨Šè‚¢‚µ‚Ü‚·B > "c:\RiKi_assistant\temp\s6_5tts_txt\%dt%_2_sjis.txt"
+
+exit
